d4f8f55d02e20c75e3d4d9ff4bc83da0d834145b
nuzulh <nuzulhikam0@gmail.com>
2022-03-19 20:04:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -16,15 +15,6 @@ function App() {
       <Events />
       <Footer />
     </>
-=======
-import './App.css';
-
-function App() {
-  return (
-    <div>
-      <h1>y</h1>
-    </div>
->>>>>>> 323ed8c55a4c3d4ab4d94459df3dbbb884424305
   );
 }
 
